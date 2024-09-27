7319074398f012f20da1a91d679c31fb64f04bcd
JusceCabrera <jusce240@gmail.com>
2024-09-27 16:20:26 -0500

    fix vercel build and add vercel.json

--- ingenieria-menu/src/pages/DataEntry/Platos.jsx
+++ ingenieria-menu/src/pages/DataEntry/Platos.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react'
 import DataEntry from '../../components/DataEntry/DataEntry'
 import '../../main.css'
-import MockData from '../../utils/mockData'
+// import MockData from '../../utils/mockData.jsx'
 import { urlServer } from '../../../utils/constantURL.js'
 import { fetchPlates } from '../../../utils/fetchsData'
 import SpinnerSVG from '../../assets/svg/spinnerSVG.svg'
